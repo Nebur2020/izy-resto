25c7bd702a5d6d86a0c7f98b0ad3f0f8f0e1751e
Hogbonouto Zinsou Kentin <66114703+kentinHogbonouto@users.noreply.github.com>
2025-02-25 11:45:49 +0100

    remove cash on checkout from select.

--- src/components/dashboard/components/payments/PaymentMethodForm.tsx
+++ src/components/dashboard/components/payments/PaymentMethodForm.tsx
@@ -69,7 +69,6 @@ const PaymentTypeSelect = ({
         'Stripe',
         'Wave',
         'Money Fusion',
-        'Paiement à la caisse',
         'Autres',
       ].map((type, index) => (
         <option key={index} value={type}>
